286634f022bcabc95f46acaa2f375569f41a7ffa
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2022-08-04 13:49:12 +0530

    booking page almost cleared

--- tutorail/home/views.py
+++ tutorail/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import departments, doctors
+from .models import departments, doctors, booking
+from .forms import BookingForm
 
 
 # Create your views here.
@@ -14,7 +15,15 @@ def about(request):
 
 
 def booking(request):
-    return render(request, 'booking.html', )
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = BookingForm()
+    dict_form = {
+        'form': form
+    }
+    return render(request, 'booking.html', dict_form)
 
 
 def contact(request):
--- tutorail/tutorail/settings.py
+++ tutorail/tutorail/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 MEDIA_URL = '/media/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
