aef3bbc68d3e59e9420bdca98e4bfead3bb9831d
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2022-11-22 15:06:18 +0530

    added try and catch block to prevent project from crashing because of temperory twilio auth_token

--- tutorail/home/views.py
+++ tutorail/home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import departments, doctors, booking
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import departments, doctors
 from .forms import BookingForm
 from twilio.rest import Client
 
@@ -23,16 +24,26 @@ def book(request):
             date = request.POST['booking_date']
             doc = request.POST['doc_name']
             Dr = doctors.objects.get(pk=doc)
-            sid = 'AC2daf97d087da75d0d9677317d68d4ef1'
-            #auth_token is temperory , it should be updated from twilio account when run this program
-            auth_token = 'b2bcd513be49e4c122cfc9ecf29fc4fb'
-            client = Client(sid, auth_token)
-            # client.messages.create(body='CITY HOSPITAL:-Your booking is successful for Date:'+date+' with Doctor: '+str(name) , from_='+14254751055', to='str(num)')
-            client.messages.create(
-                body='CITY HOSPITAL:-Hi '+name+'Your booking is successful for Date:' + date + ' with Doctor: ' + str(Dr)+'Contact Number:'+str(num),
-                from_='+14254751055', to='+919037166884')
-            print('phone number:', num, 'booked:', date, 'doctor name:', Dr)
+            try:
+                sid = 'AC2daf97d087da75d0d9677317d68d4ef1'
+                # auth_token is temporary , it should be updated from twilio account when run this program
+                auth_token = 'b2bcd513be49e4c122cfc9ecf29fc4fb'
+                client = Client(sid, auth_token)
+                # client.messages.create(body='CITY HOSPITAL:-Your booking is successful for Date:'+date+' with
+                # Doctor: '+str(name) , from_='+14254751055', to='str(num)')
+                client.messages.create(
+                    body='CITY HOSPITAL:-Hi ' + name + 'Your booking is successful for Date:' + date + ' with Doctor: ' + str(
+                        Dr) + 'Contact Number:' + str(num),
+                    from_='+14254751055', to='+919037166884')
+
+            except:
+                messages.error(request, 'auth_token is temporary , it should be updated from twilio account when run '
+                                        'this program')
+                return redirect('booking')
+
+            # print('phone number:', num, 'booked:', date, 'doctor name:', Dr)
             form.save()
+            messages.success(request, f'Booking confirm successfully')
     form = BookingForm()
 
     dict_form = {
