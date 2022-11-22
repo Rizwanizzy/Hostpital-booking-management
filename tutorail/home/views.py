84a10bba82402ce530316aa8a8f874f669824fb3
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2022-11-22 10:00:46 +0530

    booked patients name ,scheduled_date,doctor and their number will be sended to my number by default

--- tutorail/home/views.py
+++ tutorail/home/views.py
@@ -18,18 +18,19 @@ def book(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
+            name = request.POST['p_name']
             num = request.POST['p_phone']
             date = request.POST['booking_date']
             doc = request.POST['doc_name']
-            name = doctors.objects.get(pk=doc)
+            Dr = doctors.objects.get(pk=doc)
             sid = 'AC2daf97d087da75d0d9677317d68d4ef1'
             auth_token = 'b2bcd513be49e4c122cfc9ecf29fc4fb'
             client = Client(sid, auth_token)
-            #client.messages.create(body='CITY HOSPITAL:-Your booking is successful for Date:'+date+' with Doctor: '+str(name) , from_='+14254751055', to='str(num)')
+            # client.messages.create(body='CITY HOSPITAL:-Your booking is successful for Date:'+date+' with Doctor: '+str(name) , from_='+14254751055', to='str(num)')
             client.messages.create(
-                body='CITY HOSPITAL:-Your booking is successful for Date:' + date + ' with Doctor: ' + str(name),
+                body='CITY HOSPITAL:-Hi '+name+'Your booking is successful for Date:' + date + ' with Doctor: ' + str(Dr)+'Contact Number:'+str(num),
                 from_='+14254751055', to='+919037166884')
-            print('phone number:', num, 'booked:', date, 'doctor name:', name)
+            print('phone number:', num, 'booked:', date, 'doctor name:', Dr)
             form.save()
     form = BookingForm()
 
